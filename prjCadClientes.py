262b4886612c508828dc4dec26d8015f1305b34f
Gercino69 <gercino.satiro@gmail.com>
2023-12-16 18:40:38 -0300

    Commit LOCAL 2

--- prjCadClientes.py
+++ prjCadClientes.py
@@ -72,14 +72,25 @@ class Funcs(): #-> não precisa do __init__ já que esta função não cria, ser
                self.telefone_entry.insert(END, col3)
                self.cidade_entry.insert(END, col4)
      def deleta_cliente(self): #-> Função para excluir um registro de cliente
+            
             self.variaveis()
             self.conecta_bd()
-            self.cursor.execute(""" DELETE FROM clientes WHERE cod=? """, (self.codigo))
+            self.cursor.execute(""" DELETE FROM clientes WHERE cod=? """, [self.codigo])
             self.conn.commit()
 
             self.desconecta_bd()
             self.limpa_tela()
             self.select_lista()
+     def altera_cliente(self): #-> Função para altear um registro de cliente
+        self.variaveis()
+        self.conecta_bd()
+        self.cursor.execute(""" UPDATE clientes SET nome_cliente = ?, telefone = ?, cidade = ?
+                            WHERE cod = ? """, (self.nome, self.telefone, self.cidade, self.codigo))
+        self.conn.commit()
+        self.desconecta_bd()
+        self.select_lista()
+        self.limpa_tela()
+
 
 
 class Application(Funcs): #-> note que esta classe pode usar a class Funcs
@@ -91,7 +102,8 @@ class Application(Funcs): #-> note que esta classe pode usar a class Funcs
         self.widgets_frame1()  # chama função botões
         self.lista_frame2()    # chama componentes contidos no frame_2
         self.monta_tabelas()   # cria tebalas caso não exista
-        self.select_lista()
+        self.select_lista()    # preenche a GRID com registros dos clientes
+        self.Menus()           # implementa os menus
         root.mainloop()
     def tela(self):           #-> Definições para a tela
         self.root.title("Cadastro de Clientes")
@@ -118,10 +130,12 @@ class Application(Funcs): #-> note que esta classe pode usar a class Funcs
             self.bt_buscar= Button(self.frame_1, text="Buscar", bd=3, bg='#483D8B', fg='white', font=('verdana', 8, 'bold'))
             self.bt_buscar.place(relx=0.3, rely=0.1, relwidth=0.1, relheight=0.15)
             # Criação do botão Novo
-            self.bt_novo= Button(self.frame_1, text="Novo", bd=3, bg='#483D8B', fg='white', font=('verdana', 8, 'bold'), command=self.add_cliente)
+            self.bt_novo= Button(self.frame_1, text="Novo", bd=3, bg='#483D8B', fg='white'
+                                 , font=('verdana', 8, 'bold'), command=self.add_cliente)
             self.bt_novo.place(relx=0.6, rely=0.1, relwidth=0.1, relheight=0.15)
             # Criação do botão Alterar
-            self.bt_alterar= Button(self.frame_1, text="Alterar", bd=3, bg='#483D8B', fg='white', font=('verdana', 8, 'bold'))
+            self.bt_alterar= Button(self.frame_1, text="Alterar", bd=3, bg='#483D8B', fg='white'
+                                    , font=('verdana', 8, 'bold'), command=self.altera_cliente)
             self.bt_alterar.place(relx=0.7, rely=0.1, relwidth=0.1, relheight=0.15)
             # Criação do botão Apagar
             self.bt_apagar= Button(self.frame_1, text="Apagar", bd=3, bg='#483D8B', fg='white'
@@ -171,7 +185,21 @@ class Application(Funcs): #-> note que esta classe pode usar a class Funcs
          self.scroollista = Scrollbar(self.frame_2, orient='vertical')
          self.listacli.configure(yscroll=self.scroollista.set) #-> atribui o scrool ao GRID
          self.scroollista.place(relx=0.96, rely=0.1, relwidth=0.04, relheight=0.85)
-         self.listacli.bind("<Double-1>", self.OnDoubleClick)
+         self.listacli.bind("<Double-1>", self.OnDoubleClick)    
+    def Menus(self): #-> Função cria menus
+         menubar = Menu(self.root)
+         self.root.config(menu=menubar)
+         filemenu = Menu(menubar)
+         filemenu2 = Menu(menubar)
+         #-> Função dentro da função Menu, mas pode ser usada fora para reaproveitar o codigo
+         def Quit(): self.root.destroy()
+
+         menubar.add_cascade(label="Opções", menu= filemenu)
+         menubar.add_cascade(label="Sobre", menu= filemenu2)
+
+         filemenu.add_command(label="Limpa Cliente", command=self.limpa_tela)
+         filemenu.add_command(label="Sair", command=Quit)
+
 
 
 # Retorno LOOP do código --------------------------------------------
